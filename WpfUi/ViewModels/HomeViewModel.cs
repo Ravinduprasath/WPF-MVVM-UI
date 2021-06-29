3b8738668de28c0ef91c05913eb87c78e0da1442
Ravinduprasath <prasathravindu2@gmail.com>
2021-06-29 09:48:41 +0530

    first commit

---
+++ WpfUi/ViewModels/HomeViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfUi.ViewModels
+{
+    public class HomeViewModel : Conductor<object>
+    {   
+        //Load User Controls On Button Click
+        public HomeViewModel()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            ActivateItemAsync(new FirstViewModel());
+        }
+
+        public void LoadSellers() 
+        {
+            ActivateItemAsync(new SellerViewModel());
+            Content = "Sellers";
+        }
+
+        public void LoadCategories() 
+        {
+            ActivateItemAsync(new CategoriesViewModel());
+            Content = "Categories";
+        }
+
+        public void LoadFind()
+        {
+            ActivateItemAsync(new FindViewModel());
+            Content = "Find A Book";
+        }
+
+        public void LoadContact()
+        {
+            ActivateItemAsync(new ContactViewModel());
+            Content = "Contact";
+        }
+
+        public void LoadFirst()
+        {
+            ActivateItemAsync(new FirstViewModel());
+            Content = "Explore";
+        }
+
+        //Lable Content Change Dynamic
+        private string _Content ="Explore";
+
+        public string Content
+        {
+            get { return _Content; }
+            set 
+            {
+                _Content = value;
+                NotifyOfPropertyChange(() => Content);
+            }
+        }
+
+
+    }
+}
